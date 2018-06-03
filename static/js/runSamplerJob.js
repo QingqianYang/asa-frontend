91b9d4a4d4c42e498303db3b2dbc7ef0f5c66b98
QingqianYang <qingqiany.queenie@gmail.com>
2018-06-04 05:20:21 +1000

    delete the old version

--- static/js/runSamplerJob.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by zaraperumal on 9/10/16.
- */
-function hideDiv() {
-    document.getElementById('existingOrNew').style.display = "none";
-    }
-function showDiv() {
-    document.getElementById('existingOrNew').style.display = "block";
-    }
